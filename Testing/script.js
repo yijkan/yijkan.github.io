5a029e3ad763f09e577191f13fdaf89fd85bf565
Jinnie <yijkan@bergen.org>
2014-12-07 02:27:46 -0500

    testing: msg bar

--- Testing/script.js
+++ Testing/script.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+
+	$('#home').mouseenter(function() {
+		updateMB("Back to the landing page");
+	});
+
+	$('#prog').mouseenter(function() {
+		updateMB("Updates: HackRU Fall 2014 Project: Healing Humanity");
+	});
+
+	$('#mus').mouseenter(function() {
+		updateMB("Updates: SoundCloud link");
+	});
+
+	$('#econ').mouseenter(updateMB("Updates: Fed Challenge 2015"));
+
+	$('#cv').mouseenter(function() {
+		updateMB("Updates: PDF now available!");
+	});
+
+	$('.menu_item').mouseleave(function() {
+		$('#mess_bar').empty();
+		$('#mess_bar').append("Welcome to Yijin Kang's website! Hover over or click on one of the items above.");
+	});
+
 	$('.menu_item').click(function() {
 		$('.menu_item').css('background-color','#ECCAFC');
 		$(this).css('background-color','#D270FF');
@@ -24,3 +48,9 @@ $(document).ready(function() {
 		$('#content').load('prog.txt');
 	});
 });
+
+
+function updateMB (m) {
+	$('#mess_bar').empty();
+	$('#mess_bar').append(m);
+}
